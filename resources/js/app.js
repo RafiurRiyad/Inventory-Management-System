16e3e259b791d313d7f950afb7823128216d1793
RafiurRiyad <rafiurrr@gmail.com>
2022-01-23 17:17:03 +0600

    inventory intial setup done

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+//Route imported
+import {routes} from './routes';
+
+const router = new VueRouter({
+    routes,
+    mode:'history',
+  })
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
